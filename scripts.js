d322c8551cf22f6e78d992da51bd0b0c69e1e2d8
Mariusz Kosiór <mariusz.kkk@poczta.fm>
2017-08-30 23:47:21 +0200

    changed method slice on substr

---
+++ scripts.js
@@ -0,0 +1,15 @@
+var text = "Velociraptor is a genus of herbivorous ceratopsid dinosaur that first appeared during the late Maastrichtian stage of the late Cretaceous period.";
+var dinosaur = "triceratops";
+
+//content var dinosaur is uppercase
+var dinosaurUpperCase = dinosaur.toUpperCase();
+//replaced word "Velociraptor" on "TRICERATOPS"
+var textReplace = text.replace("Velociraptor", dinosaurUpperCase);
+//divorced content var textReplace on two parts and I kept length half string
+var textReplace1 = textReplace.length/2;
+
+console.log (textReplace1);
+//half string has 72 signs
+var textReplace1 = textReplace.slice(0, 72);
+
+console.log(textReplace1);
